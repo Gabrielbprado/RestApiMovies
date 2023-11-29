cac2d210d5f3d73f53861c7674504d96cb7dd5ec
Gabriel Prado <guardavege@gmail.com>
2023-11-28 22:42:57 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,34 @@
+﻿using FilmesApi.Views;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : Controller
+{
+    private static List<Movies> Movies = new List<Movies>();
+    private static int Id = 1;
+    [HttpPost]
+        public IActionResult AddMovie([FromBody] Movies Movie)
+    {
+        Movie.Id = Id++;
+        Movies.Add(Movie);
+        return CreatedAtAction(nameof(GetMovieId),
+                 new { id = Movie.Id },
+                 Movie);
+
+    }
+
+    [HttpGet]
+    public IEnumerable<Movies> GetFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50) => Movies.Skip(skip).Take(take);
+
+    [HttpGet("{id}")]
+    public IActionResult GetMovieId(int id)
+    {
+        var filme = Movies.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
+
+}
